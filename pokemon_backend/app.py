60bf1cc7afbbbbf8bbeb3014381f955b5cf0b643
Alioth Duran <duranalioth@gmai.com>
2024-10-25 23:38:13 -0400

    Updated port to deploy on railway

--- pokemon_backend/app.py
+++ pokemon_backend/app.py
@@ -1,11 +1,11 @@
 from crypt import methods
-
+import os
 from flask import Flask, jsonify
 import requests
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app) #ADD CORS TO ALLOW REQUEST FROM FRONTEND
+CORS(app)
 
 #basic route to test server
 @app.route('/')
@@ -56,5 +56,7 @@ def get_all_pokemon():
 
 
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
