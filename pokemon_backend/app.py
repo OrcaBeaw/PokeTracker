e4e749e05b27c095c563653ba7bb15cee4fe0600
Alioth Duran <duranalioth@gmai.com>
2024-10-25 23:42:04 -0400

    Added timeot to api request

--- pokemon_backend/app.py
+++ pokemon_backend/app.py
@@ -1,61 +1,52 @@
-from crypt import methods
 import os
 from flask import Flask, jsonify
 import requests
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app)  # Add CORS to allow requests from the frontend
 
-#basic route to test server
+# Basic route to test server
 @app.route('/')
 def home():
-    return 'Hello, welcome to the pokemon api!'
+    return 'Hello, welcome to the Pokémon API!'
 
-#route to get pokedata
+# Route to get specific Pokémon data
 @app.route('/pokemon/<name>', methods=['GET'])
 def get_pokemon(name):
     try:
-        #make request to PokeAPI to get data for the specified pokemon
-        response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{name}')
-        response.raise_for_status() #Raise error if request fails
-
-        #Convert response to json
+        response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{name}', timeout=10)
+        response.raise_for_status()
         pokemon_data = response.json()
-        return jsonify(pokemon_data) #return json data
-
+        return jsonify(pokemon_data)
     except requests.exceptions.RequestException as e:
-        #If an error ocurred, return a json response with an error message
         return jsonify({'error': str(e)}), 404
 
+# Route to get all Pokémon
 @app.route('/pokemon-list', methods=['GET'])
 def get_all_pokemon():
     try:
-        #request api
-        response = requests.get('https://pokeapi.co/api/v2/pokemon?limit=1025')
-        response.raise_for_status() #Raise error if request fails
+        # Request data from PokeAPI with a timeout to prevent hanging requests
+        response = requests.get('https://pokeapi.co/api/v2/pokemon?limit=1025', timeout=10)
+        response.raise_for_status()
 
-        #Convert response to json
-        pokemon_list = response.json().get('results')
+        # Convert response to JSON
+        pokemon_list = response.json().get('results', [])
 
+        # Create a list that includes name, ID, and front picture for each Pokémon
         formatted_pokemon_list = []
         for pokemon in pokemon_list:
-            pokemon_id = pokemon.get('url').split('/')[-2] #extract id from url
+            pokemon_id = pokemon.get('url').split('/')[-2]  # Extract ID from URL
             formatted_pokemon_list.append({
                 'name': pokemon.get('name'),
                 'id': pokemon_id,
-                'front_pic': f'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{pokemon_id}.png',
+                'front_pic': f'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{pokemon_id}.png'
             })
 
-        return jsonify(formatted_pokemon_list) #return json data
-
+        return jsonify(formatted_pokemon_list)  # Return JSON data containing name, ID, and image URL
 
     except requests.exceptions.RequestException as e:
-        #If an error ocurred, return a json response with an error message
-        return jsonify({'error': 'Could not retrieve Pokemon List'}), 500
-
-
-
+        return jsonify({'error': 'Could not retrieve Pokémon list'}), 500
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
